799cdac5ae485451f2dc40aef130765a2303ebbd
alex <vieru.alexandruu@gmal.com>
2023-02-21 09:24:01 +0200

    asdf

--- data.js
+++ data.js
@@ -1,3 +1,70 @@
-console.log();
-console.log(`salut!`);
-console.log(`buna ziua!`);
+// let launchReady = false;
+// let fuelLevel = 17000;
+
+// if (fuelLevel >= 20000 {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+// let launchReady = false;
+// let crewStatus = true;
+// let computerStatus = 'green';
+
+// if (crewStatus &&& computerStatus === 'green') {
+//    console.log('Crew & computer cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Crew or computer not ready!');
+//    launchReady = false;
+// }
+
+// if (launchReady) {
+//    console.log(("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+//    console.log("Fed parrot...");
+//    console.log("Ignition...");
+//    console.log("Liftoff!");
+// } else {
+//    console.log("Launch scrubbed.");
+// }
+
+// let launchReady = false;
+// let fuelLevel = 17000;
+
+// if (fuellevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+
+let launchReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
